3e72a8d8a62c36dc3451ac4809f44b67dd6bc06f
herdialfachri <alfachri75@gmail.com>
2024-09-06 17:51:45 +0700

    installing myth, configuring, and migrating

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'mherdialfachri@ummi.ac.id';
+    public string $fromName   = 'Simpakal App';
     public string $recipients = '';
 
     /**
